b0e152530b20f891e695931d9b13e15081bdd5de
Xiaoou Pan <xip024@ucsd.edu>
2021-02-23 00:55:35 -0800

    Simulation for Huber regression

--- R/opl.R
+++ R/opl.R
@@ -14,5 +14,10 @@ X = matrix(rnorm(n * p), n, p)
 err = rnorm(n)
 Y = beta0 + X %*% beta + err
 
-fit = linearReg(X, Y, alpha = 0.1)
-fit$coeff
+fit = linearReg(X, Y, alpha = 0.01)
+fit$coeff[1:(s + 1)]
+max(fit$coeff[(s + 2):(p + 1)])
+
+fit.huber = huberReg(X, Y, alpha = 0.01)
+fit.huber$coeff[1:(s + 1)]
+max(fit.huber$coeff[(s + 2):(p + 1)])
