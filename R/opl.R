373a039042f6ee7ce3449f22deb7e1d2cc0bc402
Xiaoou Pan <xip024@ucsd.edu>
2021-02-23 21:09:34 -0800

    Comparison with Lasso

--- R/opl.R
+++ R/opl.R
@@ -1,6 +1,7 @@
 rm(list = ls())
 
 library(glmnet)
+library(MASS)
 Rcpp::sourceCpp('src/opl.cpp')
 
 n = 200
@@ -15,9 +16,22 @@ err = rnorm(n)
 Y = beta0 + X %*% beta + err
 
 fit = linearReg(X, Y, alpha = 0.01)
-fit$coeff[1:(s + 1)]
-max(fit$coeff[(s + 2):(p + 1)])
+beta.hat = fit$coeff[-1]
+beta.hat[1:s]
+max(abs(beta.hat[-(1:s)]))
+norm(beta.hat - beta, "2")
 
 fit.huber = huberReg(X, Y, alpha = 0.01)
-fit.huber$coeff[1:(s + 1)]
-max(fit.huber$coeff[(s + 2):(p + 1)])
+beta.hat.huber = fit.huber$coeff[-1]
+beta.hat.huber[1:s]
+max(abs(beta.hat.huber[-(1:s)]))
+norm(beta.hat.huber - beta, "2")
+
+
+### glmnet
+fit.lasso = cv.glmnet(X, Y)
+beta.lasso = coef(fit.lasso, s = "lambda.min")[-1]
+beta.lasso [1:s]
+max(abs(beta.lasso [-(1:s)]))
+norm(beta.lasso  - beta, "2")
+
